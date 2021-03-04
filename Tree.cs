44c4689b91d2d2fe501abf7c00fed0be07ddfcb0
Thomas Stanger <thomasstanger0@gmail.com>
2021-03-04 15:40:09 +0000

    Add project files.

---
+++ Tree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters;
+
+namespace BinaryTree
+{
+    public class Tree<T>
+    {
+        private readonly TreeNode<T> _root;
+        public Tree(T inputData)
+        {
+            if (inputData == null)
+            {
+                throw new ArgumentNullException("Null not allowed");
+            }
+            _root = new TreeNode<T>(inputData);
+        }
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            foreach (var child in children)
+            {
+                _root.AddChild(child._root);
+            }
+        }
+        
+        private void Print(TreeNode<T> root, string spaces)
+        {
+            if (this._root == null)
+            {
+                return;
+            }
+            Console.WriteLine(spaces + root.Value);
+            TreeNode<T> child = null;
+            for (var i = 0; i < root.ChildrenCount; i++)
+            {
+                child = root.GetChild(i);
+                Print(child, spaces + " ");
+            }
+        }
+        public void Traverse()
+        {
+            Print(_root, string.Empty);
+        }
+    }
+
+}
---
+++ TreeNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    public class TreeNode<T>
+    {
+        private bool hasParent;
+        private readonly List<TreeNode<T>> _childNodes;
+        public TreeNode(T inputData)
+        {
+            if (inputData == null)
+            {
+                throw new ArgumentNullException("Null not allowed");
+            }
+            Value = inputData;
+            _childNodes = new List<TreeNode<T>>();
+        }
+      
+        public T Value { get; set; }
+
+        public int ChildrenCount => _childNodes.Count;
+
+        public void AddChild(TreeNode<T> child)
+        {
+            if (child == null)
+            {
+                throw new ArgumentNullException("Null not allowed");
+            }
+            if (child.hasParent)
+            {
+                throw new ArgumentException("The node already has a parent!");
+            }
+            child.hasParent = true;
+            _childNodes.Add(child);
+        }
+        
+        public TreeNode<T> GetChild(int index)
+        {
+            return _childNodes[index];
+        }
+    }
+}
